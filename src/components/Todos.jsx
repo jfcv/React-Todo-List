e3e84c9c26af3d7934c033977ae18c062e8086e9
velveet <velveet@pm.me>
2020-09-02 11:48:33 -0500

    rendering todos elements on the Todos component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,11 +3,10 @@ import React, { Component } from 'react'
 export class Todos extends Component {
     render() {
         console.log(this.props.todos);
-        return (
-            <div>
-                <h1>Todos</h1>
-            </div>
-        )
+        return this.props.todos.map((todo) => (
+            <h3>{ todo.title }</h3>
+        ));
+        
     }
 }
 
