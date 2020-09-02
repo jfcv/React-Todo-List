bd7670b4ee944de59b5ae681256029c0e22a6ce4
velveet <velveet@pm.me>
2020-09-02 09:06:16 -0500

    App component changed as a class component to passing the state. 'state' created. console log the state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-      <Todos />
-    </div>
-  );
+
+export class App extends Component {
+
+  state = {
+    todos: [
+        {
+            id: 1,
+            title: 'Take out the trash',
+            completed: false
+        },
+        {
+            id: 2,
+            title: 'Dinner with wife',
+            completed: false
+        },
+        {
+            id: 3,
+            title: 'Meeting with boss',
+            completed: false
+        },
+    ]
+  }
+
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div>
+        <Todos />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
