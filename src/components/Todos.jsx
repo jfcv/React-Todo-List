2a459011957dce9b81e5f531441a35bcb0ccb53b
velveet <velveet@pm.me>
2020-09-02 11:56:59 -0500

    TodoItem component added. TodoItem component imported on Todos component. Passing down props.

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
+import TodoItem from './TodoItem'
 
 export class Todos extends Component {
     render() {
         console.log(this.props.todos);
         return this.props.todos.map((todo) => (
-            <h3>{ todo.title }</h3>
+            <TodoItem key={todo.id} todo={todo} />
         ));
-        
     }
 }
 
