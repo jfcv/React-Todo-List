6175ccb87d47b56d5db32105a5a26476e29b7f52
velveet <velveet@pm.me>
2020-09-03 14:35:04 -0500

    Links to the Routes '/' & '/about' added. Links styled.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function AddTodo() {
     return (
         <header style={headerStyle}>
             <h1>Todo List</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link> 
         </header>
     )
 }
@@ -14,4 +16,8 @@ const headerStyle = {
     background: '#5D1411',
     padding: '10px'
 }
- 
+
+const linkStyle = {
+    color: '#fff',
+    fontFamily: 'monospace',
+}
